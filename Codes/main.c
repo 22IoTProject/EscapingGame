61ef79f021101150f899672ed4f1cfad999ecac6
zerow41 <59548168+zerow41@users.noreply.github.com>
2022-06-08 17:05:33 +0900

    Update FinalCode

--- Codes/main.c
+++ Codes/main.c
@@ -5,13 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ioctl.h>
-#include <sys/signal.h>
-#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <termios.h>
-#include <time.h>
 #include <unistd.h>
 
 #define dot "/dev/dot"
@@ -19,34 +15,35 @@
 #define fnd "/dev/fnd"
 #define clcd "/dev/clcd"
 
-unsigned char obstacle[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-unsigned char player[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x10};
-unsigned char matrix[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //출력할 매트릭스
-unsigned char CNTD_TBL[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8,
-                            0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E,
-                            0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x89};
-bool b = true; // 게임 종료 판단
-int play_time[4] = {0,0,0,0};
-unsigned char fnd_num[4];
+unsigned char obstacle[8] = 
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+unsigned char player[8] = 
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x10}; // 2*1 형태의 플레이어
+unsigned char matrix[8] = 
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //출력할 매트릭스
+unsigned char CNTD_TBL[10] = 
+    {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+int play_time[4] = {0,0,0,0}; // 카운트다운 용 배열
+unsigned char fnd_num[4]; // fnd 용 배열
+int time_show = 30; // 시간 제한 초기화 값
+int count_timer = 0; // 시간 유지 시간
 int total_score = 0; // 점수
-char score_text[32]; // 점수 clcd
-int time_show = 30;              // 시간 제한 초기화 값
-int count_timer = 0;             // 시간 유지 시간
+char score_text[16]; // 점수 clcd
+bool b = true; // 게임 종료 판단
 
 
-// 카운트다운 counter_timer동안 time_show 유지
-// ****** count_timer 300000으로 수정하기
+// 카운트다운 함수, count_timer동안 time_show 유지
 void fnd_control(int fnd_d) {
     int tenth = time_show / 10;
 
     if (time_show == 0) {
         b = false;
-    } else if (count_timer != 50000 && time_show > 0) {
+    } else if (count_timer != 200000 && time_show > 0) {
         play_time[2] = tenth;
         play_time[3] = time_show - tenth * 10;
 
         count_timer++;
-    } else if (count_timer == 50000 && time_show > 0) {
+    } else if (count_timer == 200000 && time_show > 0) {
         play_time[2] = tenth;
         play_time[3] = time_show - tenth * 10;
 
@@ -61,10 +58,10 @@ void fnd_control(int fnd_d) {
     write(fnd_d, &fnd_num, sizeof(fnd_num));
 }
 
-// 점수 계산
+// 점수 반환 함수
 char *score_count(int total_score) {
     char tmp[16] = "Score: ";
-    char score[5]; // 점수 string 만들기 위한 배열
+    char score[5] = ""; // 점수 string 만들기 위한 배열
 
     if (total_score > 999){
         strcpy(score, "999+");
@@ -78,7 +75,7 @@ char *score_count(int total_score) {
     return score_text;
 }
 
-// clcd 출력
+// clcd 출력 함수
 void clcd_text(char clcd_text[]) {
     int clcd_d;
     
@@ -91,13 +88,13 @@ void clcd_text(char clcd_text[]) {
     close(clcd_d);
 }
 
-//최종배열을 만드는 함수
+// 플레이어+장애물 배열 함수
 int setMatrix(char d1[], char d2[], int d) // d1이 장애물
 {
     int h;
-    int b = 1;
+    bool b = true;
     for (h = 0; h < 8; h++) {
-        if ((d2[h] & d1[h]) > 0) //비트연산자로 충돌하면 끝
+        if ((d2[h] & d1[h]) > 0)
         {
             total_score--;
             if (total_score <0)
@@ -113,43 +110,40 @@ int setMatrix(char d1[], char d2[], int d) // d1이 장애물
 }
 
 int main() {
-    struct timeval dotstart, dotend, tactstart, tactend, obstaclestart,obstacleend, obstacle_spst, obstacle_spend, timest, fndstart, fndend, clcdstart, clcdend;
+    struct timeval dotstart, dotend, tactstart, tactend, obstaclestart,obstacleend, obstacle_spst, obstacle_spend, fndstart, fndend;
     int dot_d = 0;
     int tact = 0;
     int fnd_d = 0;
     int clcd_d = 0;
-    unsigned char t = 0;
-    int k, obstime;
-    double obstacle_p; //장애물 위치
-    int obstacle_n, i; //장애물 길이
-    int clcdtest = true;
-    int exit = 1;
-    char re[32] = "Restart key 5 ..";
+    unsigned char t = 0; // 입력받는 tact 값
+    double obstacle_p; // 장애물 위치
+    int obstacle_n, i; // 장애물 길이
+    int exit = 1; // 재시작 안하면 exit
+    char re[32]; // 재시작 문구 출력용
     
-    k = 10;
     srand(time(NULL)); // 매번 다른 장애물 출현하도록 rand
     
     gettimeofday(&obstaclestart, NULL);
     gettimeofday(&obstacle_spst, NULL);
     gettimeofday(&dotstart, NULL);
-    gettimeofday(&timest, NULL);
 
-    while(exit){ // restart를 위한 while문
+    while(exit){ // 재시작을 위한 while문
         // 시작 시 2번 키 입력
         if (tact == 0) {
             tact = open(tact_d, O_RDWR);
         }
         gettimeofday(&tactstart, NULL);
-    
+
+        // 안내 문구 출력
         clcd_text("Start key 2 ... left | - | right ");
         while (1) {
             gettimeofday(&tactend, NULL);
             read(tact, &t, sizeof(t));
             
-            if (t == 2) {
+            if (t == 2) { // 2번 버튼 입력 시 게임 시작 문구 출력
                 printf("2\n");
                 tact = close(tact);
-                clcd_text("Hide Score..^^  Play!");
+                clcd_text("Hide Score..    Play Time!");
                 break;
             }
         }
@@ -165,35 +159,40 @@ int main() {
                 break;
             }
 
-            // 조건문 수정 필요
-            if ((obstacleend.tv_usec + 2000000 - obstaclestart.tv_usec > 2000000) && ((obstacleend.tv_sec - obstaclestart.tv_sec) == 2)) {
+            // 2초마다 장애물 생성
+            if ((obstacleend.tv_usec + 2000000 - obstaclestart.tv_usec > 2000000) 
+                && ((obstacleend.tv_sec - obstaclestart.tv_sec) == 2)) 
+            {
                 obstacle_n = rand() % 2;
-                if (obstacle_n == 0) //블록길이 2
+                if (obstacle_n == 0) // 장애물 길이 2
                 {
-                    obstacle_p = rand() % 6; //블록 생성 위치 결정
-                    obstacle[0] = pow(2.0, obstacle_p) + pow(2.0, obstacle_p + 1.0f); //정해진 위치에 블록 생성
-                } else //블록길이 3
+                    obstacle_p = rand() % 6; // 장애물 생성 위치 결정
+                    obstacle[0] = pow(2.0, obstacle_p) + pow(2.0, obstacle_p + 1.0f); // 해당 위치에 장애물 생성
+                } else // 장애물 길이 3
                 {
-                    obstacle_p = rand() % 5;
-                    obstacle[0] = pow(2.0, obstacle_p) + pow(2.0, obstacle_p + 1.0f) + pow(2.0, obstacle_p + 2.0f);
+                    obstacle_p = rand() % 5; 
+                    obstacle[0] = pow(2.0, obstacle_p) 
+                                    + pow(2.0, obstacle_p + 1.0f) 
+                                    + pow(2.0, obstacle_p + 2.0f);
                 }
                 gettimeofday(&obstaclestart, NULL);
             }
+            
             gettimeofday(&obstacle_spend, NULL);
-    
-            if ((obstacle_spend.tv_usec - obstacle_spst.tv_usec > 800000) || (obstacle_spend.tv_sec > obstacle_spst.tv_sec && (obstacle_spend.tv_usec + 1000000 - obstacle_spst.tv_usec > 800000))) {
+            // 0.8초 간격으로 장애물이 내려올 수 있도록
+            if ((obstacle_spend.tv_usec - obstacle_spst.tv_usec > 800000) 
+                || (obstacle_spend.tv_sec > obstacle_spst.tv_sec 
+                    && (obstacle_spend.tv_usec + 1000000 - obstacle_spst.tv_usec > 800000))
+            ) {
                 if (!setMatrix(obstacle, player, dot_d)) {
                     break;
                 } else { 
-                    for (i = 6; i > -1; i--) //떨어지는 모습 구현
+                    for (i = 6; i > -1; i--) // 장애물 떨어지도록
                     {
                         obstacle[i + 1] = obstacle[i];
-                        /*if (i==0) {
-                            total_score++;
-                        }*/
                     }
                 }
-                if (obstacle[0] > 0) //블록 생성 전 삭제
+                if (obstacle[0] > 0) // 장애물 초기화
                 {
                     obstacle[0] = 0;
                 }
@@ -201,7 +200,7 @@ int main() {
             }
             
     
-        // dot matrix와 tack switch를 0.2초마다 번갈아가면서 접근
+        // dot matrix, tack switch, fnd를 0.2초마다 번갈아가면서 접근
             if ((dotend.tv_usec - dotstart.tv_usec > 200000) || (dotend.tv_sec > dotstart.tv_sec && (dotend.tv_usec + 1000000 - dotstart.tv_usec > 200000))) {
                 dot_d = close(dot_d);
                 
@@ -212,23 +211,24 @@ int main() {
         
                 while (1) {
                     gettimeofday(&tactend, NULL);
-                    read(tact, &t, sizeof(t)); // tact switch에 0.2초간 접근해있는 동안 입력받음
-        
-                    switch (t) {
-                        case 4:
-                            if (player[6] != 0x80) // 4번 버튼 입력시 왼쪽으로 플레이어 이동
-                            {
-                                player[6] = player[6] << 1;
-                                player[7] = player[7] << 1; 
-                            }
-                            break;
-                        case 6:
-                            if (player[6] != 0x01) // 6번 버튼 입력시 오른쪽으로 플레이어 이동
-                            {
-                                player[6] = player[6] >> 1;
-                                player[7] = player[7] >> 1;
-                            }
-                            break;
+                    read(tact, &t, sizeof(t)); 
+
+                    if (t==1 || t==4 || t==7 || t==10){ // 왼쪽으로 플레이어 이동
+                        if (player[6] != 0x80)
+                        {
+                            player[6] = player[6] << 1;
+                            player[7] = player[7] << 1; 
+                            total_score++;
+                        }
+                        break;
+                    } else if (t==3 || t==6 || t==9 || t==12){ // 오른쪽으로 플레이어 이동
+                        if (player[6] != 0x01) 
+                        {
+                            player[6] = player[6] >> 1;
+                            player[7] = player[7] >> 1;
+                            total_score++;
+                        }
+                        break;
                     }
         
                 // 0.2초 경과 or tact switch 입력이 있는 경우 tact switch에 접근 해제
@@ -237,6 +237,7 @@ int main() {
                         break;               
                     }
                 }
+                gettimeofday(&dotstart, NULL);
     
                 // 제한 시간 출력
                 if (fnd_d == 0) {
@@ -248,30 +249,31 @@ int main() {
                     gettimeofday(&fndend, NULL);
                     fnd_control(fnd_d);
         
-                    // 0.2초 경과시 fnd에 접근 해제
-                    if ((fndend.tv_usec - fndstart.tv_usec > 200000) || (fndend.tv_sec > fndstart.tv_sec && (fndend.tv_usec + 1000000 - fndstart.tv_usec > 200000))) {
+                    // 0.1초 경과시 fnd에 접근 해제
+                    if ((fndend.tv_usec - fndstart.tv_usec > 100000) || (fndend.tv_sec > fndstart.tv_sec && (fndend.tv_usec + 1000000 - fndstart.tv_usec > 100000))) {
                         fnd_d = close(fnd_d);
                         break;
                     }
                 }
-                gettimeofday(&dotstart, NULL);
             }
         }
+        
         // 게임 종료 시 score 출력하고 restart 물어보기
         if (!b) {
             if (tact == 0) {
                 tact = open(tact_d, O_RDWR);
             }
             gettimeofday(&tactstart, NULL);
-            
-            strcat(re,score_count(total_score));
+
+            strcpy(re, "Restart key 5 ..");
+            strcat(re, score_count(total_score)); // "restart + score"
             clcd_text(re);
             
             while (1) {
                 gettimeofday(&tactend, NULL);
                 read(tact, &t, sizeof(t));
                 
-                if (t == 5) {
+                if (t == 5) { // 재시작 및 변수 초기화
                     printf("RE\n");
                     time_show = 30;
                     total_score = 0;
@@ -279,12 +281,14 @@ int main() {
                     tact = close(tact);
                     break;
                 }
-                else if (t==8){
+                else if (t == 8){ // 게임 종료
+                    clcd_text("Exit Game");
+                    printf("Game Over\n");
                     exit = 0;
+                    tact = close(tact);
                     break;
                 }
             }
         }
     }
 }
-
